dd36ec8ed4400382091fe7919459ebbd80cd23b0
memet1968konya <mehmetbahar196842@gmail.com>
2026-08-04 09:22:25 +0000

    V2 APK iç sürümü 2.0.1 olarak düzeltildi

--- android-v2/app/build.gradle.kts
+++ android-v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.poweron.navigation.v2"
         minSdk = 24
         targetSdk = 36
-        versionCode = 200
-        versionName = "2.0.0"
+        versionCode = 201
+        versionName = "2.0.1"
     }
 
     buildTypes {
